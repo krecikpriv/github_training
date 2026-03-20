cec970e531ed3f4787d8258f47b15e2fade5f301
krecikpriv <przemaz89@o2.pl>
2026-03-20 13:08:07 +0100

    update

--- TERRAFORM/01/main.tf
+++ TERRAFORM/01/main.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_droplet" "main" {
-  name   = "piotr-koska-github-actions-droplet"
+  name   = "przemek-mazurek-github-actions-droplet"
   region = "fra1"
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-24-04-x64"
