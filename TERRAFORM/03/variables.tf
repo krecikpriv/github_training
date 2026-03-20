de9b8d4d8935548724b7a620e258bfd9f5631a81
krecikpriv <przemaz89@o2.pl>
2026-03-20 14:43:41 +0100

    update

--- TERRAFORM/03/variables.tf
+++ TERRAFORM/03/variables.tf
@@ -17,7 +17,7 @@ variable "project_environment" {
 variable "own_ssh_public_key" {
   description = "This is your own SSH public key - this is not secret, but we want to validate it"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDkGgVpbqoMqfMK7nevVqjjwXue1T1vaiuZ6UQRUVaCQRn6hGyBc5ekyiOqLsvnc58KdkvO9cv3CQrrLX38XWtG5dW9H90vIq1tvVwkMqD2lh0t1dexCox3dpQIO5K+8SCqB4PTdNTMtrZdjj6nNqlR8OqyJSPPtIeqJMqlbyU3dgW+GNgllgfS2EmqRRGFiE5USvVGaxyFVk06Df4bV4bzp7KeJ1qXGy9dYx5AczTGeFIKEmJpCRe7g3pe9XhsEpkNu8lxBjaXmKX+ZD3KPyewbwvqXDsKfo68JegYFGsUCRfQe1Lqg5dwgRJKRB4BVxjsOrQ5LSXzJtT/PiJaeXbB rsa-key-20260320"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAy6kre8AvOIE4cg1VHCm7N2hphmdtLyDariTJG9l33p eddsa-key-20260320"
 
   validation {
     condition     = can(regex("^ssh-(rsa|ed25519)\\s+[A-Za-z0-9+/]+={0,3}(\\s+.+)?$", var.own_ssh_public_key))
